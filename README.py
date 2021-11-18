ed4bf52c0294ac0ab8fb105a237085e33f88f49d
YuriyPyter <94583115+YuriyPyter@users.noreply.github.com>
2021-11-18 06:25:46 +0300

    Update README.py

--- README.py
+++ README.py
@@ -1,10 +1,1 @@
-# Python
-a = 1
-b = 2
-c = 3
-print(a, b, c)
-number_1 = int(input('Введите первое число: '))
-number_2 = int(input('Введите второе число: '))
-string_1 = input('Напишите первое слово: ')
-string_2 = input('Напишите второе слово: ')
-print(f'{number_1}, {number_2}, {string_1}, {string_2}')
+
